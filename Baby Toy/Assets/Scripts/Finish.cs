73c4a8acbfc83f89d546e19dca3ab73c3f40ce2e
Adam Bennett <adam18099@gmail.com>
2021-09-06 17:44:05 -0600

    added particles and finish script

---
+++ Baby Toy/Assets/Scripts/Finish.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Finish : MonoBehaviour
+{
+    public ParticleSystem particles;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        particles.Play();
+    }
+}
