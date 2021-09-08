0d0f813f21635840ce022aaa86d503a8c19ae5f2
Yarnuh <jaggerinyars@gmail.com>
2021-09-08 11:12:21 -0600

    Beginning of State Machine

---
+++ Baby Toy/Assets/Scripts/StandingState.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StandingState : State
+{
+    public StandingState(StateController stateController) : base(stateController) { }
+
+    public override void CheckTransitions()
+    {
+        //if (!stateController.CheckIfInRange("Player"))
+        //{
+        //    stateController.SetState(new CryState(stateController));
+        //}
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            stateController.SetState(new WalkState(stateController));
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            stateController.SetState(new ChaseState(stateController));
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            stateController.SetState(new PickUpState(stateController));
+        }
+        //if (Input.GetKeyDown(KeyCode.R))
+        //{
+        //    stateController.SetState(new StandingState(stateController));
+        //}
+    }
+    public override void Act()
+    {
+        //if (stateController.enemyToChase != null)
+        //{
+        //    stateController.destination = stateController.enemyToChase.transform;
+        //    stateController.ai.SetTarget(stateController.destination);
+        //}
+    }
+    public override void OnStateEnter()
+    {
+        stateController.isWalking = false;
+        stateController.isChasing = false;
+        stateController.isPickingUp = false;
+
+        stateController.isStanding = true;
+    }
+}
---
+++ Baby Toy/Assets/Scripts/StateController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StateController : MonoBehaviour
+{
+    public State currentState;
+
+    public bool isWalking = false;
+    public bool isChasing = false;
+    public bool isPickingUp = false;
+    public bool isStanding = false;
+
+    void Start()
+    {
+        SetState(new WalkState(this));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentState.CheckTransitions();
+        currentState.Act();
+    }
+
+    public void SetState(StateController state)
+    {
+        if (currentState != null)
+        {
+            currentState.OnStateExit();
+        }
+
+        currentState = state;
+        gameObject.name = "AI agent in state " + state.GetType().Name;
+
+        if (currentState != null)
+        {
+            currentState.OnStateEnter();
+        }
+    }
+}
